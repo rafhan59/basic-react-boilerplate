3c71c763910ca36c9e35235e8da5cf7502f045db
rafhan59 <rafhan5659@gmail.com>
2024-04-09 22:13:01 +0600

    Basic Boilerplate for React Development

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-red-600 font-bold text-6xl">Rafhan&apos;s Basic React Setup</h1>
+    </>
+  )
+}
+
+export default App
